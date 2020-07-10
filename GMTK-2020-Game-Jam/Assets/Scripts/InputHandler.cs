acb5a5a3dc41f0feea153c89f5861d7371971a2c
Wesley <wesley.verhagen@student.fontys.nl>
2020-07-10 22:15:06 +0200

    feat: Added input handling

---
+++ GMTK-2020-Game-Jam/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputHandler : MonoBehaviour
+{
+    public UnityEvent LeftClick = new UnityEvent();
+    public UnityEvent RigthClick = new UnityEvent();
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetAxis("Fire1") != 0)
+        {
+            LeftClick.Invoke();
+        }
+        if (Input.GetAxis("Fire2") != 0)
+        {
+            RigthClick.Invoke();
+        }
+    }
+}
