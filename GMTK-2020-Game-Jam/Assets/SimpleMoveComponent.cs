3b940cd796165a9ecfe59290c39ea1049421a51e
MindHawk <timgooren@hotmail.nl>
2020-07-10 23:02:25 +0200

    feat: add stuff to scene, create a spawner

---
+++ GMTK-2020-Game-Jam/Assets/SimpleMoveComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMoveComponent : MonoBehaviour
+{
+    public Transform playerTransform;
+    public int speed;
+    private Rigidbody2D _rb;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 newPosition = Vector2.MoveTowards(_rb.position, playerTransform.position, speed * Time.deltaTime);
+        _rb.MovePosition(newPosition);
+    }
+}
---
+++ GMTK-2020-Game-Jam/Assets/SpawnEnemies.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+
+    public float spawnDelay = 5;
+    public Transform playerTransform;
+
+    private float _timeToNextSpawn;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _timeToNextSpawn = spawnDelay;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (_timeToNextSpawn < 0)
+        {
+            SpawnEnemy();
+            _timeToNextSpawn += spawnDelay;
+        }
+
+        _timeToNextSpawn -= Time.deltaTime;
+    }
+
+    private void SpawnEnemy()
+    {
+        SimpleMoveComponent enemy = Instantiate(objectToSpawn, transform.position, Quaternion.identity).GetComponent<SimpleMoveComponent>();
+        enemy.playerTransform = playerTransform;
+    }
+}
