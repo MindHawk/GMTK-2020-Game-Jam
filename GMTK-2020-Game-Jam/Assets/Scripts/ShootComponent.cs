fa4d1023cb958041a73c8f5d60da9bd3642e4752
Wesley <wesley.verhagen@student.fontys.nl>
2020-07-10 22:50:38 +0200

    feat: added barrel recoil

---
+++ GMTK-2020-Game-Jam/Assets/Scripts/ShootComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootComponent : MonoBehaviour
+{
+    private Rigidbody2D parentRigidBody;
+    public float torque = 3;
+
+    private void Awake()
+    {
+        parentRigidBody = GetComponentInParent<Rigidbody2D>();
+    }
+    public void Shoot()
+    {
+        parentRigidBody.AddTorque(torque, ForceMode2D.Impulse);
+    }
+}
