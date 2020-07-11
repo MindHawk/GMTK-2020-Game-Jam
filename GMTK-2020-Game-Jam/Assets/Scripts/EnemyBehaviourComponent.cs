f001266cf71887b157d43afb5db5913b7356a593
Wesley <wesley.verhagen@student.fontys.nl>
2020-07-11 13:34:41 +0200

    feat: enemies explode on death

--- GMTK-2020-Game-Jam/Assets/Scripts/EnemyBehaviourComponent.cs
+++ GMTK-2020-Game-Jam/Assets/Scripts/EnemyBehaviourComponent.cs
@@ -6,11 +6,18 @@ public class EnemyBehaviourComponent : MonoBehaviour
 {
     [SerializeField]
     private string AffectedTag;
+    [SerializeField]
+    private ParticleSystem DeathParticle;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag(AffectedTag))
         {
             Destroy(this.gameObject);
         }
+    }
+
+    private void OnDestroy()
+    {
+        Instantiate(DeathParticle, transform.position, Quaternion.identity);
     }
 }
