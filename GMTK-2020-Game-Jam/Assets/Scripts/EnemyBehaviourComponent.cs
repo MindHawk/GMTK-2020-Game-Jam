d150adf5cf3abe67d336b20c9c1601070209498e
Wesley <wesley.verhagen@student.fontys.nl>
2020-07-11 10:50:15 +0200

    feat: player can now kill enemies

---
+++ GMTK-2020-Game-Jam/Assets/Scripts/EnemyBehaviourComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviourComponent : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "PlayerProjectile")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
