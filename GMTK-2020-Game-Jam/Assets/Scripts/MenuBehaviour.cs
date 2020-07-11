be4004539b52771b2390c152962195631c46fecd
Wesley <wesley.verhagen@student.fontys.nl>
2020-07-11 14:03:27 +0200

    feat: added game over menu functionality

---
+++ GMTK-2020-Game-Jam/Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    public void SwitchScenes(string targetScene)
+    {
+        SceneManager.LoadScene(targetScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
