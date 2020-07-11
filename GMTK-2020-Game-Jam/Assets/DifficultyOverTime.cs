0c8394a15de425d0a6c6fde6ecaba4acfea40c56
MindHawk <timgooren@hotmail.nl>
2020-07-11 20:59:26 +0200

    feat: add difficulty over time, initial difficulty settings

---
+++ GMTK-2020-Game-Jam/Assets/DifficultyOverTime.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SpawnerSettings
+{
+    public List<SpawnChance> objectsToSpawn;
+    public float spawnDelay = 7;
+    public float spawnVariance = 3;
+    public float difficultyDuration;
+}
+
+public class DifficultyOverTime : MonoBehaviour
+{
+    public List<SpawnerSettings> difficultyLevels;
+
+    private float _difficultyActiveDuration;
+    private int _activeDifficulty = 0;
+    [SerializeField]
+    private List<SpawnEnemies> spawners;
+
+    private bool finalDifficulty = false;
+
+    private void Start()
+    {
+        _activeDifficulty = -1;
+    }
+    
+    private void Update()
+    {
+        if (finalDifficulty) return;
+        _difficultyActiveDuration -= Time.deltaTime;
+        if (_difficultyActiveDuration <= 0)
+        {
+            _activeDifficulty++;
+            _difficultyActiveDuration += difficultyLevels[_activeDifficulty].difficultyDuration;
+            ChangeSpawnerDifficulty(_activeDifficulty);
+            if (difficultyLevels.Count - 1 == _activeDifficulty)
+            {
+                finalDifficulty = true;
+            }
+        }
+    }
+
+    private void ChangeSpawnerDifficulty(int difficulty)
+    {
+        foreach (SpawnEnemies spawner in spawners)
+        {
+            spawner.spawnDelay = difficultyLevels[difficulty].spawnDelay;
+            spawner.spawnVariance = difficultyLevels[difficulty].spawnVariance;
+            spawner.objectsToSpawn = difficultyLevels[difficulty].objectsToSpawn;
+        }
+    }
+}
