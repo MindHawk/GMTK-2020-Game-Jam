54a3a15706431d4b0163138e7df96d78abb38972
Wesley <wesley.verhagen@student.fontys.nl>
2020-07-12 14:14:26 +0200

    feat: cannons are now loaded based on player prefs

---
+++ GMTK-2020-Game-Jam/Assets/Scripts/Player/CannonManagerComponent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonManagerComponent : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ShipCannonParent;
+    [SerializeField]
+    private GameObject AirCannonParent;
+    [SerializeField]
+    private List<GameObject> ShipCannons;
+    [SerializeField]
+    private List<GameObject> AirCannons;
+
+    private void Awake()
+    {
+        LoadCannons();
+    }
+
+    private void LoadCannons()
+    {
+        LoadShipCannon();
+        LoadAirCannon();
+    }
+
+    private void LoadShipCannon()
+    {
+        Instantiate(ShipCannons[PlayerPrefs.GetInt("ShipCannon")], ShipCannonParent.transform);
+    }
+
+    private void LoadAirCannon()
+    {
+        Instantiate(AirCannons[PlayerPrefs.GetInt("AirCannon")], AirCannonParent.transform);
+
+    }
+
+}
