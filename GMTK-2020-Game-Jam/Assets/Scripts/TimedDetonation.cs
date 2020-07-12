4e7c02e554232381a96fa77c721e85600bb84419
Wesley <wesley.verhagen@student.fontys.nl>
2020-07-12 13:22:37 +0200

    feat: added new cannon types

---
+++ GMTK-2020-Game-Jam/Assets/Scripts/TimedDetonation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDetonation : MonoBehaviour
+{
+    [SerializeField]
+    private float TimeToLive = 10;
+    [SerializeField]
+    private ParticleSystem DetonationEffect;
+    [SerializeField]
+    private float DetonationDuration;
+    [SerializeField]
+    private float DetonationRange;
+    private bool hasDetonated = false;
+
+    private float _timeLeft;
+    void Start()
+    {
+        _timeLeft = TimeToLive;
+    }
+
+    void Update()
+    {
+        _timeLeft -= Time.deltaTime;
+        if (_timeLeft <= 0 && !hasDetonated)
+        {
+            Detonate();
+        }
+    }
+
+    private void Detonate()
+    {
+        hasDetonated = true;
+        DetonationEffect.Play();
+        GetComponent<CircleCollider2D>().radius = DetonationRange;
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        Destroy(gameObject, DetonationDuration);
+    }
+}
