e361474a0e0fdf773caeb817f66fa46320fbc504
MindHawk <timgooren@hotmail.nl>
2020-07-12 16:31:47 +0200

    feat: better customisation menu

--- GMTK-2020-Game-Jam/Assets/Scripts/Menu/CustomisationBehaviour.cs
+++ GMTK-2020-Game-Jam/Assets/Scripts/Menu/CustomisationBehaviour.cs
@@ -2,31 +2,68 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CustomisationBehaviour : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI HighScoreText;
-    [SerializeField]
-    private TMP_Dropdown ShipDropdown;
-    [SerializeField]
-    private TMP_Dropdown AirDropdown;
+
+    private int _selectedShipButton;
+    private int _selectedAirButton;
+
+    [SerializeField] private Image shipButton1;
+    [SerializeField] private Image shipButton2;
+    [SerializeField] private Image shipButton3;
+    
+    [SerializeField] private Image airButton1;
+    [SerializeField] private Image airButton2;
+    [SerializeField] private Image airButton3;
+
+    [SerializeField] private Sprite selectedSprite;
 
     private void Awake()
     {
         HighScoreText.text = "High Score: " + PlayerPrefs.GetInt("HighScore");
-        ShipDropdown.value = PlayerPrefs.GetInt("ShipCannon");
-        AirDropdown.value = PlayerPrefs.GetInt("AirCannon");
-
+        _selectedShipButton = PlayerPrefs.GetInt("ShipCannon");
+        _selectedAirButton = PlayerPrefs.GetInt("AirCannon");
+        switch (_selectedShipButton)
+        {
+            case 0:
+                shipButton1.sprite = selectedSprite;
+                break;
+            case 1:
+                shipButton2.sprite = selectedSprite;
+                break;
+            case 2:
+                shipButton3.sprite = selectedSprite;
+                break;
+            default:
+                break;
+        }
+        switch (_selectedAirButton)
+        {
+            case 0:
+                airButton1.sprite = selectedSprite;
+                break;
+            case 1:
+                airButton2.sprite = selectedSprite;
+                break;
+            case 2:
+                airButton3.sprite = selectedSprite;
+                break;
+            default:
+                break;
+        }
     }
 
-    public void UpdateShipCannonSelection()
+    public void UpdateShipCannonSelection(int value)
     {
-        PlayerPrefs.SetInt("ShipCannon", ShipDropdown.value);
+        PlayerPrefs.SetInt("ShipCannon", value);
     }
 
-    public void UpdateAirCannonSelection()
+    public void UpdateAirCannonSelection(int value)
     {
-        PlayerPrefs.SetInt("AirCannon", AirDropdown.value);
+        PlayerPrefs.SetInt("AirCannon", value);
     }
 }
