9aebdc46e7b7b48a81aabcf5514e9b02cad8d886
Wesley <wesley.verhagen@student.fontys.nl>
2020-07-12 14:37:53 +0200

    feat: cannons can now be swapped from main menu

--- GMTK-2020-Game-Jam/Assets/Scripts/Menu/CustomisationBehaviour.cs
+++ GMTK-2020-Game-Jam/Assets/Scripts/Menu/CustomisationBehaviour.cs
@@ -7,8 +7,26 @@ public class CustomisationBehaviour : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI HighScoreText;
+    [SerializeField]
+    private TMP_Dropdown ShipDropdown;
+    [SerializeField]
+    private TMP_Dropdown AirDropdown;
+
     private void Awake()
     {
         HighScoreText.text = "High Score: " + PlayerPrefs.GetInt("HighScore");
+        ShipDropdown.value = PlayerPrefs.GetInt("ShipCannon");
+        AirDropdown.value = PlayerPrefs.GetInt("AirCannon");
+
+    }
+
+    public void UpdateShipCannonSelection()
+    {
+        PlayerPrefs.SetInt("ShipCannon", ShipDropdown.value);
+    }
+
+    public void UpdateAirCannonSelection()
+    {
+        PlayerPrefs.SetInt("AirCannon", AirDropdown.value);
     }
 }
