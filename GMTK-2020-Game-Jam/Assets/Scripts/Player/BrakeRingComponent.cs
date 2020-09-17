47269611c400bdee0d89536a1de4e0a8f913aa94
Wesley <wesley.verhagen@student.fontys.nl>
2020-09-17 15:36:16 +0200

    Feat: Added Brake Ring

---
+++ GMTK-2020-Game-Jam/Assets/Scripts/Player/BrakeRingComponent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrakeRingComponent : ShootComponent
+{
+    [Header("Braking")]
+    [SerializeField]
+    private float PassiveBrakingForce;
+    [SerializeField]
+    private float ActiveBrakingForce;
+    [SerializeField]
+    private float ActiveBrakingTime;
+    [Header("Visuals")]
+    [SerializeField]
+    private List<ParticleSystem> BrakingParticles;
+
+    private Rigidbody2D parentRigidbody;
+    private void Start()
+    {
+        parentRigidbody = player.gameObject.GetComponent<Rigidbody2D>();
+        parentRigidbody.angularDrag = PassiveBrakingForce;
+    }
+    public override void Shoot()
+    {
+        StartCoroutine(ActiveBrake());
+    }
+
+    private IEnumerator ActiveBrake()
+    {
+        float currentBrakingTime = 0;
+        foreach (ParticleSystem particle in BrakingParticles)
+        {
+            particle.Play();
+        }
+        while (currentBrakingTime < ActiveBrakingTime)
+        {
+            parentRigidbody.angularDrag = ActiveBrakingForce;
+            currentBrakingTime += Time.deltaTime;
+            yield return null;
+        }
+        parentRigidbody.angularDrag = PassiveBrakingForce;
+        foreach (ParticleSystem particle in BrakingParticles)
+        {
+            particle.Stop();
+        }
+    }
+}
